24c781c9ed10da499831b59029e90236023575c2
MATHEUS EGON SCHNEIDER <matheus_schneider150@estudante.sesisenai.org.br>
2026-06-24 16:04:14 -0300

    tables-Cotacao_Personalizadas

---
+++ Bloco-4/03_tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Solicitacao_SLA (
+    id_solicitacao INT AUTO_INCREMENT PRIMARY KEY,
+    id_oportunidade INT NOT NULL,
+    id_parceiro INT NOT NULL,
+    data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (id_oportunidade) REFERENCES Oportunidade_CRM(id_oportunidade),
+    FOREIGN KEY (id_parceiro) REFERENCES Parceiros(id_parceiro)
+);
+
+CREATE TABLE Cotacao_Personalizadas (
+    id_cotacao INT AUTO_INCREMENT PRIMARY KEY,
+    id_oportunidade INT NOT NULL,
+    id_pacote INT,
+    valor_total_calculado DECIMAL(10, 2),
+    status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (id_oportunidade) REFERENCES Oportunidade_CRM(id_oportunidade),
+    FOREIGN KEY (id_pacote) REFERENCES Pacote(id_pacote)
+);
