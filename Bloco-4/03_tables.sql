745affd48dff33556db59a0bde9d9c947353c30b
MATHEUS EGON SCHNEIDER <matheus_schneider150@estudante.sesisenai.org.br>
2026-07-01 16:38:57 -0300

    ultimas 4 tabelas feitas

--- Bloco-4/03_tables.sql
+++ Bloco-4/03_tables.sql
@@ -46,3 +46,42 @@ CREATE TABLE Contrato_Digital (
     status VARCHAR(50) NOT NULL DEFAULT 'Assinado',
     FOREIGN KEY (id_proposta) REFERENCES Propostas_Comerciais(id_proposta)
 );
+
+CREATE TABLE Viagem (
+    id_viagem INT AUTO_INCREMENT PRIMARY KEY,
+    id_contrato INT NOT NULL UNIQUE,
+    data_embarque TIMESTAMP NOT NULL,
+    data_retorno TIMESTAMP NOT NULL,
+    status_viagem VARCHAR(50) NOT NULL DEFAULT 'Confirmada',
+    FOREIGN KEY (id_contrato) REFERENCES Contrato_Digital(id_contrato)
+);
+
+
+CREATE TABLE Pagamento_Contrato (
+    id_pagamento INT AUTO_INCREMENT PRIMARY KEY,
+    id_contrato INT NOT NULL,
+    metodo_pagamento VARCHAR(50) NOT NULL,
+    valor_total DECIMAL(10, 2) NOT NULL,
+    numero_parcela INT NOT NULL DEFAULT 1, 
+    total_parcelas INT NOT NULL DEFAULT 1,
+    status_transacao VARCHAR(50) NOT NULL DEFAULT 'Pendente',
+    FOREIGN KEY (id_contrato) REFERENCES Contrato_Digital(id_contrato)
+);
+
+CREATE TABLE Consentimentos_LGPD (
+    id_consentimento INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    tipo_consentimento VARCHAR(100) NOT NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'Ativo',
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+);
+
+CREATE TABLE Log_Acesso (
+    id_log INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario_interno INT NOT NULL,
+    id_cliente INT,
+    tipo_operacao VARCHAR(50) NOT NULL,
+    data_acesso TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_usuario_interno) REFERENCES Usuario_Interno(id_usuario_interno),
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+);
